f852d200986800c3e86d1c1459b73f86036595c5
Meynin <mey09.nin"gmail.com>
2022-10-12 12:38:35 +0700

    Master Bridging BPJS

---
+++ tests/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * 
+ */
+$vclaim_config = [
+    'cons_id'       => '123456',
+    'secret_key'    => 'abcdef',
+    'user_key'      => 'xxxyyyzzz',
+    'base_url'      => 'https://apijkn-dev.bpjs-kesehatan.go.id',
+    'service_name'  => 'vclaim-rest-dev'
+];
+
+//aplicaresws
+$aplicare_config = [
+    'cons_id'      => '',
+    'secret_key'   => '',
+    'base_url'     => 'https://new-api.bpjs-kesehatan.go.id:8080',
+    'service_name' => 'aplicaresws/rest'
+];
+
+
+//pcare
+$pcare_config = [
+    'cons_id'      => '',
+    'secret_key'   => '',
+    'base_url'     => 'https://dvlp.bpjs-kesehatan.go.id:9081',
+    'service_name' => 'pcare-rest-v3.0',
+    'pcare_user'   => '',
+    'pcare_pass'   => '',
+    'kd_aplikasi'  => ''
+];
